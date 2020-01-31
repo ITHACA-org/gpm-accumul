99608789de2e09e5f27e415c66e3593a44db2e29
Simone Balbo <simone.blb@gmail.com>
2020-01-31 17:38:33 +0100

    Update credentials.py

--- gpm_repo/credentials.py
+++ gpm_repo/credentials.py
@@ -2,3 +2,5 @@ user = 'username'
 passwd = 'password'
 
 DATADIR = '/path/to/the/gpm/data/directory'
+THRESH_ADJ_ABSPATH = '/path/to/the/threshold/adjustment/raster'
+THRESHOLDS_ABSPATH = '/path/to/the/threshold/values/file'
